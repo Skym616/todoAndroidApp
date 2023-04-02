a71e501e72f2c6cd1070ab8fe4b9ff6b3c1846a7
Skym616 <skym616@pop-os.localdomain>
2023-04-02 17:38:32 +0100

    wip

--- app/src/main/java/com/skym/todoapp/MainActivity.kt
+++ app/src/main/java/com/skym/todoapp/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.skym.todoapp
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import com.skym.todoapp.presentation.MainScreen
 import com.skym.todoapp.presentation.theme.TodoAppTheme
 
@@ -10,6 +16,15 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val appCenterKey =  BuildConfig.APPCENTER_KEY
+        AppCenter.start(
+            application,
+            appCenterKey,
+            Crashes::class.java,
+            Analytics::class.java
+        )
+
         setContent {
             TodoAppTheme {
                 MainScreen()
